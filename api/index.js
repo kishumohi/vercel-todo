5b9c2bd137d64987c7573581ef74e6235a3b9d3f
Kishan Kalavadiya <36093612+kishumohi@users.noreply.github.com>
2025-02-23 13:48:03 +0530

    restrict CORS to specific origin for enhanced security

--- api/index.js
+++ api/index.js
@@ -8,7 +8,11 @@ const port = 3000 || process.env.PORT;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://vercel-todo-kappa.vercel.app"],
+  })
+);
 // MongoDB connection
 const mongoURI = process.env.MONGO_URI;
 mongoose
